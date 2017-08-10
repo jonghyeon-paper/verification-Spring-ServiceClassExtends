401eae2006febc0185076b1bbf00cfcaf1598cd9
good <good@good-pc>
2017-08-10 11:34:35 +0900

    제너릭 사용

--- src/main/java/spring/hello/service/Hello3ServiceAbstract.java
+++ src/main/java/spring/hello/service/Hello3ServiceAbstract.java
@@ -2,10 +2,8 @@ package spring.hello.service;
 
 import java.util.List;
 
-import spring.hello.model.Users;
+public abstract class Hello3ServiceAbstract<T> {
 
-public abstract class Hello3ServiceAbstract {
-
-	public abstract List<Users> getUserList();
-	public abstract List<String> getUserRoleList(String userId);
+	public abstract List<T> getUserList();
+	public abstract List<T> getUserRoleList(String userId);
 }
